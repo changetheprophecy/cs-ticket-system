ec3a3cb55b11666678459c6ce2c9dbb8a35a4ec5
Atasi Sharma <atasi2002@gmail.com>
2025-09-27 14:13:45 +0600

    added navbar and footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  
+  )
+}
+
+export default App
